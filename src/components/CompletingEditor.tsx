213bb6be73811e938f8fa1e7a2f0bd8ef63e357a
Howard Zuo <someotherusername@gmail.com>
2017-02-06 17:34:02 -0800

    intermediate work on transitioning to plain draft

--- src/components/CompletingEditor.tsx
+++ src/components/CompletingEditor.tsx
@@ -1,89 +1,93 @@
-import {EditorState} from "draft-js";
-import createMentionPlugin, {defaultSuggestionsFilter} from "draft-js-mention-plugin";
-import Editor from "draft-js-plugins-editor";
+import {Editor, EditorState} from "draft-js";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import {connect} from "react-redux";
-import {Dispatch} from "redux";
 
-import {INITIAL_COMPLETIONS} from "../state/initialState";
-import {Completions, setEditorState, setSearchText, State} from "../state/state";
-
-interface CompletingEditorProps {
-  completions: Completions;
-  editorState: EditorState;
-  peopleSuggestions: any;
-  hashtagsSuggestions: any;
-  relationsSuggestions: any;
-  onChange: (editorState: EditorState) => void;
-  onSearchChangePeople: (searchText: string) => void;
-  onSearchChangeHashtags: (searchText: string) => void;
-  onSearchChangeRelations: (searchText: string) => void;
+interface CompletingEditorState {
+  currentEditorState: EditorState;
 }
 
-const mapStateToProps = (state: State): any => ({
-  completions: state.completions,
-  editorState: state.editorState,
-  peopleSuggestions: defaultSuggestionsFilter(state.searchTexts.get("@"), INITIAL_COMPLETIONS.get("@")),
-  hashtagsSuggestions: defaultSuggestionsFilter(state.searchTexts.get("#"), INITIAL_COMPLETIONS.get("#")),
-  relationsSuggestions: defaultSuggestionsFilter(state.searchTexts.get("<>"), INITIAL_COMPLETIONS.get("<>")),
+interface TriggerSpec {
+  trigger: string;
+  beforeTriggerAllowed: RegExp;
+  afterTriggerAllowed: RegExp;
+}
+
+interface MatchString {
+  triggerSpec: TriggerSpec;
+  contentBlockKey: string;
+  triggerOffset: number;
+  caretOffset: number;
+  value: string;
+}
+
+const getMatchStringForTriggerSpec = (triggerSpec: TriggerSpec) => (editorState: EditorState): MatchString | null => {
+  const selectionState = editorState.getSelection();
+  if (selectionState.isCollapsed()) {
+    const contentBlockKey = selectionState.getStartKey();
+    const caretOffset = selectionState.getStartOffset();
+    const contentState = editorState.getCurrentContent();
+    const contentBlock = contentState.getBlockForKey(contentBlockKey);
+    const contentBlockText = contentBlock.getText();
+    const triggerStart = contentBlockText.indexOf(triggerSpec.trigger);
+    if (triggerStart != -1) {
+      const triggerEnd = triggerStart + triggerSpec.trigger.length;
+      if (triggerEnd <= caretOffset) {
+        const beforeTrigger = contentBlockText.slice(0, triggerStart);
+        const afterTrigger = contentBlockText.slice(triggerEnd, caretOffset);
+        const beforeGood = triggerSpec.beforeTriggerAllowed.test(beforeTrigger);
+        const afterGood = triggerSpec.afterTriggerAllowed.test(afterTrigger);
+        if (beforeGood && afterGood) {
+          return {
+            triggerSpec, contentBlockKey, caretOffset,
+            triggerOffset: triggerStart,
+            value: afterTrigger
+          };
+        }
+      }
+    }
+  }
+  return null;
+};
+
+const getMatchStringForTrigger = (trigger: string) => getMatchStringForTriggerSpec({
+  trigger,
+  beforeTriggerAllowed: /(^|.*\s)$/g,
+  afterTriggerAllowed: /.*/g,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<State>): any => ({
-  onChange: (editorState: EditorState) => dispatch(setEditorState(editorState)),
-  onSearchChangePeople: (searchText: any) => dispatch(setSearchText("@", searchText.value)),
-  onSearchChangeHashtags: (searchText: any) => dispatch(setSearchText("#", searchText.value)),
-  onSearchChangeRelations: (searchText: any) => {
-    console.assert(searchText.value.charAt(0) === ">");
-    const mySearchText = searchText.value.substring(1);
-    dispatch(setSearchText("<>", mySearchText));
-  },
-});
+export class CompletingEditor extends React.Component<{}, CompletingEditorState> {
+  constructor() {
+    super();
+    this.state = {
+      currentEditorState: EditorState.createEmpty(),
+    };
+  }
 
-const mentionPluginPeople = createMentionPlugin({
-  mentionTrigger: "@",
-  mentionPrefix: "@",
-  entityMutability: "SEGMENTED",
-});
-const MentionSuggestionsPeople = mentionPluginPeople.MentionSuggestions;
-const mentionPluginHashtags = createMentionPlugin({
-  mentionTrigger: "#",
-  mentionPrefix: "#",
-  entityMutability: "IMMUTABLE",
-});
-const MentionSuggestionsHashtags = mentionPluginHashtags.MentionSuggestions;
-const mentionPluginRelations = createMentionPlugin({
-  mentionTrigger: "<>",
-  mentionPrefix: "<>",
-  entityMutability: "IMMUTABLE",
-});
-const MentionSuggestionsRelations = mentionPluginRelations.MentionSuggestions;
-const plugins = [mentionPluginPeople, mentionPluginHashtags, mentionPluginRelations];
-
-class CompletingEditorComponent extends React.Component<CompletingEditorProps, undefined> {
   public render() {
+    const selectionState = this.state.currentEditorState.getSelection();
     return (
-      <div className="editor-container">
-        <Editor
-          editorState={this.props.editorState}
-          onChange={this.props.onChange}
-          plugins={plugins}
-        />
-        <MentionSuggestionsPeople
-          onSearchChange={this.props.onSearchChangePeople}
-          suggestions={this.props.peopleSuggestions}
-        />
-        <MentionSuggestionsHashtags
-          onSearchChange={this.props.onSearchChangeHashtags}
-          suggestions={this.props.hashtagsSuggestions}
-        />
-        <MentionSuggestionsRelations
-          onSearchChange={this.props.onSearchChangeRelations}
-          suggestions={this.props.relationsSuggestions}
-        />
+      <div>
+        <div className="editor-container">
+          <Editor
+            editorState={this.state.currentEditorState}
+            onChange={this.onEditorStateChange}
+          />
+        </div>
+        <div>
+          {JSON.stringify(getMatchStringForTrigger("@")(this.state.currentEditorState))}
+        </div>
+        <div>
+          {JSON.stringify(getMatchStringForTrigger("#")(this.state.currentEditorState))}
+        </div>
+        <div>
+          {JSON.stringify(getMatchStringForTrigger("<>")(this.state.currentEditorState))}
+        </div>
       </div>
     );
   }
-}
 
-export const CompletingEditor = connect(mapStateToProps, mapDispatchToProps)(CompletingEditorComponent);
+  private onEditorStateChange = (editorState: EditorState): void => {
+    this.setState({
+      currentEditorState: editorState,
+    });
+  }
+}
